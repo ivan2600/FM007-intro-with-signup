bfef58b2acf1e90aba3246d03f2e06790624d648
Ivan Adamo <ivan.adamo.8@gmail.com>
2023-12-31 17:51:00 -0300

    logica desarrollada

--- main.js
+++ main.js
@@ -7,16 +7,22 @@ const alertImg = document.querySelectorAll('.alert-img');
 const alertMsg = document.querySelectorAll('.alert-msg');
 
 
-
+  //por cada INPUT
   input.forEach( ( cadaInput , i )=>{
+    //Escucho el CLICK
     claim.addEventListener('click', (e)=>{
+      //Evito actualizar la pagina en cada CLICK
       e.preventDefault();
-      
+      //Patron de comparacion de mail
       const pattern =  /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+      //Si el valor de MAIL esta VACIO
       if (mail.value == "") {
+        //Agrega estas clases 
         alertImg[i].classList.remove('active');
         alertMsg[i].classList.remove('active');
+        //
       } else if (!mail.value.match(pattern)) {
+        //Agrega estas clases 
         alertImgMail.classList.add('active');
         alertMsgMail.classList.add('active');
       } else {
@@ -24,15 +30,18 @@ const alertMsg = document.querySelectorAll('.alert-msg');
       }
 
       console.log('esaa');
+      //Borrar TODAS las clases
       alertImg[i].classList.remove('active');
       alertMsg[i].classList.remove('active');
-      
+      //Si los INPUT estan VACIOS
       if (input[i].value == "") {
+        //Agrega estas clases
         alertImg[i].classList.add('active');
         alertMsg[i].classList.add('active');
       }
-
+      //Si TODOS los inputs NO estan VACIOS Y el MAIL esta BIEN escrito
       if (input[i].value != "" && mail.value.match(pattern)) {
+        //CAMBIA EL SUBMIT A GRACIAS
         claim.value = 'THANK YOU!';
       }
       
